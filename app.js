5cd9e604d1ec5a689ac67723961178f772eeddbc
mihajlovskakate <mihajlovskakate1@gmail.com>
2022-11-15 11:11:51 +0100

    Functional app

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector(".wrapper"),
 inputPart = document.querySelector(".input-part"),
-inputCity = inputPart.querySelector("input");
+inputCity = inputPart.querySelector("input"),
+weatherPart = wrapper.querySelector(".weather-part"),
+arrowBack = wrapper.querySelector("header i");
 
 
 let api;
@@ -20,6 +22,34 @@ function requestApi(city){
     fetchData();
 }
 
+
+function fetchData(){ 
+    fetch(api)
+        .then(res => res.json())
+        .then(info => {
+            const city = info.name;
+            const country = info.sys.country;
+            const temp = info.main.temp;
+            const description = info.weather[0].description;
+            const feelsLike = info.main.feels_like;
+            const humidity = info.main.humidity;
+            const iconImg = info.weather[0].icon;
+        
+          
+        
+            weatherPart.querySelector(".temp .number").innerText = Math.floor(temp);
+            weatherPart.querySelector(".weather").innerText = description;
+            weatherPart.querySelector(".location span").innerText = `${city}, ${country}`;
+            weatherPart.querySelector(".temp .number-2").innerText = Math.floor(feelsLike);
+            weatherPart.querySelector(".humidity span").innerText = `${humidity}`;
+            weatherPart.querySelector("img").src = `http://openweathermap.org/img/wn/${iconImg}@2x.png`;
+            wrapper.classList.add("active");
+
+            arrowBack.addEventListener("click", ()=>{
+                wrapper.classList.remove("active");
+            })
+        })
+}
 
 
 
